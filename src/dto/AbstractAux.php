8a2faba3bcee9753b25973243d9200615068bcd8
flavio <flavio@flavio>
2016-10-25 22:44:29 -0200

    .

--- src/dto/AbstractAux.php
+++ src/dto/AbstractAux.php
@@ -15,11 +15,17 @@ abstract class AbstractAux {
 	}
 	
 	public function getDTO($key):ObjectDTO {
+		if(is_null($key))
+			return null;
+		
 		$bo = $this->crud->getEntity($key);
 		return $this->parseDTO ($bo);
 	}
 
 	public function getBO($key):ObjectBO {
+		if(is_null($key))
+			return null;
+		
 		return $this->crud->getEntity($key);
 	}
 
@@ -39,6 +45,9 @@ abstract class AbstractAux {
 	}
 	
 	public function create($dto):ObjectDTO {
+		if(is_null($dto))
+			return null;
+		
 		$bo = $this->parseNewBO($dto);
 		$bo = $this->crud->createEntity($bo);
 		
@@ -46,14 +55,32 @@ abstract class AbstractAux {
 	}
 	
 	public function update($dto){
+		if(is_null($dto))
+			return null;
+		
 		$bo = $this->crud->getEntity($dto->id);
 		$this->updateBO($dto,$bo);
 		$this->crud->updateEntity($bo);
 	}
 	
 	public function delete($key) {
+		if(is_null($key))
+			return;
+		
 		$bo = $this->crud->getEntity($key);
 		$this->crud->deleteEntity($bo);
+	}
+	
+	public function parseDTOs($bos):array {
+		if(is_null($bos))
+			return null;
+		
+		$dtos = array();
+		foreach($bos as $bo) {
+			array_push($dtos, $this->parseDTO($bo));
+		}
+		
+		return $dtos;
 	}
 	
 	protected abstract function parseNewBO($dto):ObjectBO;
