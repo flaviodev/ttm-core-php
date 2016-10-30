af29a0974509fdb3cab21092cff668c49e21c384
flavio <flavio@flavio>
2016-10-29 23:53:25 -0200

    documentation comment

--- src/control/AbstractFrontController.php
+++ src/control/AbstractFrontController.php
@@ -4,11 +4,57 @@ namespace ttm\control;
 use ttm\util\UtilDate;
 use ttm\util\Util;
 
-abstract class AbstractFrontController extends Rest {
+/**
+ * AbstractFrontController - Reuseble component that encapsulates treatment requisitions
+ * of web services. Using the pattern of friendly URL (ex:/contextaplication/service/method) 
+ * where the parametters are send by POST http method. 
+ *
+ * @author flaviodev - Flávio de Souza TTM/ITS - fdsdev@gmail.com
+ * 
+ * @package ttm-core-php
+ * @namespace ttm\control
+ * @abstract
+ * @version 1.0
+ */
+abstract class AbstractFrontController {
+	
+	/**
+	 * @property $configFile - has the configuration of dependences of services/commands 
+	 * interfaces and yours concrete classes.
+	 * 
+	 * @static @access private 
+	 * @since 1.0 
+	 */
 	private static $configFile = null;
+
+	/**
+	 * @property $controllers - it's a flyweight pattern that store the concrete services 
+	 * (facades) associated with your respective interfaces, as it is in the configuration file.
+	 * 
+	 * @static @access private
+	 * @since 1.0 
+	 */
 	private static $controllers = array();
+
+	/**
+	 * @property $commands - it's a flyweight pattern that store the concrete commands
+	 * associated with your respective names, as it is in the configuration file.
+	 *
+	 * @static @access private
+	 * @since 1.0
+	 */
 	private static $commands = array();
 
+	
+	/**
+	 * @method getConfig - encapsulates the invocation of the abstract method that returns 
+	 * the properties of configuration file on concrete implementation of the front controller 
+	 *
+	 * @return object that contains the configuration file properties  
+	 * 
+	 * @access public
+	 * @since 1.0 
+	 */
 	private function getConfig() {
 		if(AbstractFrontController::$configFile==null) {
 			AbstractFrontController::$configFile=$this->getConfigInfo();
@@ -16,7 +62,16 @@ abstract class AbstractFrontController extends Rest {
 		
 		return AbstractFrontController::$configFile;
 	}
-	
+
+	/**
+	 * @method getControllerImplementation - this method is responsible for seeking on 
+	 * controllers configuration what is the concrete service to the given interface,
+	 * keeping the services already requested. 
+	 *
+	 * @param $interfaceName - name of service interface for seeking the concrete service configurated
+	 * @access protected
+	 * @since 1.0
+	 */
 	protected function getControllerImplementation($interfaceName) {
 		if(isset(AbstractFrontController::$controllers[$interfaceName])) {
 			return AbstractFrontController::$controllers[$interfaceName];
@@ -32,7 +87,17 @@ abstract class AbstractFrontController extends Rest {
 	
 		return null;
 	}
-	
+
+	/**
+	 * @method getCommandsImplementation - this method is responsible for seeking on
+	 * commands configuration what is the concrete command to the given command name,
+	 * keeping the commands already requested. In this case, there is just one common interface
+	 * for all commands, so the seeking is for name and not for interface.
+	 *
+	 * @param $commandName - name of command for seeking the concrete command configurated
+	 * @access protected
+	 * @since 1.0
+	 */
 	protected function getCommandsImplementation($commandName) {
 		if(isset(AbstractFrontController::$commands[$commandName])) {
 			return AbstractFrontController::$commands[$commandName];
@@ -49,9 +114,23 @@ abstract class AbstractFrontController extends Rest {
 		return null;
 	}
 	
+	/**
+	 * @method processRequest - when the client makes a server request, the .htaccess should be
+	 * redirect the request to a concrete implementation of this class. The first part of the
+	 * URL is project that implments the front controller, the secont part contains the sevice 
+	 * or command required. This second part is divide into two other: class and method that will
+	 * be instantiated and invoked. This method separates the parts of requested URL, creates
+	 * the class and invokes the method.
+	 *
+	 * @access public
+	 * @since 1.0
+	 */
 	public function processRequest()
 	{
+		//name of a service interface or command(fixed text)
 		$parameter1 = null;
+		
+		//name of the service method or name of concrete command  
 		$parameter2 = null;
 		
 		$partsOfRequest = str_getcsv($_REQUEST['request'],"/");
@@ -67,12 +146,14 @@ abstract class AbstractFrontController extends Rest {
 		}
 		
 		$return = null;
-		if(strcasecmp("command", $parameter1)==0) {
+		// the first parammeter defines the type of process
+		if(strcasecmp("command", $parameter1)===0) {
 			$return = $this->processCommand($parameter2);
 		} else {
 			$return = $this->processController($parameter1, $parameter2);
 		}
 		
+		// TODO create an uncoupled component for manipulating data of request/reply
 		if(!is_null($return)) {
 			$data = array();
 			$data['reply']= $this->filterSendPropertiesReply($return);
@@ -80,68 +161,37 @@ abstract class AbstractFrontController extends Rest {
 			echo json_encode($data,false,JSON_UNESCAPED_UNICODE);
 		}
 	}
-	
-	private function filterSendPropertiesReply($data) {
-		if(is_array($data) || is_subclass_of($data, \ArrayAccess::class)) {
-			$arrayFO = array();
-			foreach ($data as $item) {
-				array_push($arrayFO, $this->getFilteredObjectSendPropertiesReply($item));
-			}
-			
-			return $arrayFO; 
-		} else {
-			return $this->getFilteredObjectSendPropertiesReply($data);
-		}
-	}
-	
-	private function getFilteredObjectSendPropertiesReply($data) {
-		if(is_null($data))
-			return;
-		
-		$filteredObject = new \stdClass();
-		
-		$reflectionObject = new \ReflectionObject($data);
-		
-		foreach ($reflectionObject->getProperties() as $prop) {
-			if(strpos($prop->getDocComment(), "@ttm-DtoAttribute")>-1) {
-				$property = $prop->getName();
-				
-				$function = Util::doMethodName($property,"get");
-				if((int)method_exists($data,$function) > 0) {
-					$reflectionMethod = new \ReflectionMethod($data, $function);
-					
-					$value = $reflectionMethod->invoke($data, null);
-					
-					if(is_a($value, \DateTime::class)) {
-						$value = UtilDate::dateToString($value);
-					}
-				
-					$filteredObject->$property = $value; 
-				} else {
-					$function = Util::doMethodName($property,"is");
-					if((int)method_exists($data,$function) > 0) {
-						$reflectionMethod = new \ReflectionMethod($data, $function);
-							
-						$value = $reflectionMethod->invoke($data, null);
-						$filteredObject->$property = $value;
-					}
-				}
-			}
-		}
-		
-		return $filteredObject;
-	}
 
+	/**
+	 * @method processController - encapsulates the locale of controller  
+	 *
+	 * @access public
+	 * @since 1.0
+	 */
 	private function processController($controllerName, $actionName) {
 		$controller = $this->locateController($controllerName);
 		return $this->invoke($controller, $actionName);		
 	}
 
+	/**
+	 * @method constructor of class
+	 *
+	 * @param array $options - has the options and configurations for
+	 * @access public
+	 * @since 1.0
+	 */
 	private function processCommand($commandName) {
 		$command = $this->locateCommand($commandName);
 		return $this->invoke($command, "execute", true);
 	}
-	
+
+	/**
+	 * @method constructor of class
+	 *
+	 * @param array $options - has the options and configurations for
+	 * @access public
+	 * @since 1.0
+	 */
 	private function invoke($object, $function,bool $isCommand=false) {
 		if(is_null($object)) {
 			$this->response('',404);
@@ -173,12 +223,109 @@ abstract class AbstractFrontController extends Rest {
 			}
 		}
 	}
+
+	/**
+	 * @method getFilteredObjectSendPropertiesReply - encapsulate a parsing mechanism for conversting
+	 * a model class to a dto class, obtaing the attribuites of model class using refletion tools.
+	 * The filter process is to separate what the attributes that will be sent on reply, dont sending
+	 * for examples attribuites used on collection mapping (orm). This filter uses a anotation in
+	 * attributes (@ttm-DtoAttribute) that should be sent.
+	 *
+	 * @param model object for 'filtering'and convertion
+	 * @return new object (dto) with just the marked attributes
+	 *
+	 * @access private
+	 * @since 1.0
+	 */
+	private function getFilteredObjectSendPropertiesReply($data) {
+		if(is_null($data))
+			return;
+	
+			$filteredObject = new \stdClass();
+	
+			$reflectionObject = new \ReflectionObject($data);
+	
+			foreach ($reflectionObject->getProperties() as $prop) {
+				if(strpos($prop->getDocComment(), "@ttm-DtoAttribute")>-1) {
+					$property = $prop->getName();
+	
+					$function = Util::doMethodName($property,"get");
+					if((int)method_exists($data,$function) > 0) {
+						$reflectionMethod = new \ReflectionMethod($data, $function);
+							
+						$value = $reflectionMethod->invoke($data, null);
+							
+						if(is_a($value, \DateTime::class)) {
+							$value = UtilDate::dateToString($value);
+						}
+	
+						$filteredObject->$property = $value;
+					} else {
+						$function = Util::doMethodName($property,"is");
+						if((int)method_exists($data,$function) > 0) {
+							$reflectionMethod = new \ReflectionMethod($data, $function);
+	
+							$value = $reflectionMethod->invoke($data, null);
+							$filteredObject->$property = $value;
+						}
+					}
+				}
+			}
+	
+			return $filteredObject;
+	}
+	
+	/**
+	 * @method filterSendPropertiesReply - method verifies if the parameter is a model object or
+	 * an array of objects and delegates to method: getFilteredObjectSendPropertiesReply
+	 *
+	 * @param model object for 'filtering'and convertion
+	 * @return new object (dto) with just the marked attributes
+	 *
+	 * @access private
+	 * @since 1.0
+	 */
+	private function filterSendPropertiesReply($data) {
+		if(is_array($data) || is_subclass_of($data, \ArrayAccess::class)) {
+			$arrayFO = array();
+			foreach ($data as $item) {
+				array_push($arrayFO, $this->getFilteredObjectSendPropertiesReply($item));
+			}
+				
+			return $arrayFO;
+		} else {
+			return $this->getFilteredObjectSendPropertiesReply($data);
+		}
+	}
 	
 	
+	/**
+	 * @method constructor of class
+	 *
+	 * @param array $options - has the options and configurations for
+	 * @access public
+	 * @since 1.0
+	 */
 	public abstract function locateController($interfaceName);
+
+
+	/**
+	 * @method constructor of class
+	 *
+	 * @param array $options - has the options and configurations for
+	 * @access public
+	 * @since 1.0
+	 */
 	public abstract function locateCommand($interfaceName);
+
+	/**
+	 * @method constructor of class
+	 *
+	 * @param array $options - has the options and configurations for
+	 * @access public
+	 * @since 1.0
+	 */
 	public abstract function getConfigInfo();
 	
 }
 
-?>
--- src/control/Rest.php
+++ src/control/Rest.php
@@ -2,9 +2,9 @@
 namespace ttm\control;
 
 /*
-* File : Rest.inc.php
-* Author : Arun Kumar Sekar
-*/
+ * File : Rest.inc.php
+ * Author : Arun Kumar Sekar
+ */
 class Rest {
 	public $_allow = array();
 	public $_content_type = "application/json";
@@ -30,47 +30,47 @@ class Rest {
 	}
 	private function get_status_message() {
 		$status = array(
-			100 => 'Continue',
-			101 => 'Switching Protocols',
-			200 => 'OK',
-			201 => 'Created',
-			202 => 'Accepted',
-			203 => 'Non-Authoritative Information',
-			204 => 'No Content',
-			205 => 'Reset Content',
-			206 => 'Partial Content',
-			300 => 'Multiple Choices',
-			301 => 'Moved Permanently',
-			302 => 'Found',
-			303 => 'See Other',
-			304 => 'Not Modified',
-			305 => 'Use Proxy',
-			306 => '(Unused)',
-			307 => 'Temporary Redirect',
-			400 => 'Bad Request',
-			401 => 'Unauthorized',
-			402 => 'Payment Required',
-			403 => 'Forbidden',
-			404 => 'Not Found',
-			405 => 'Method Not Allowed',
-			406 => 'Not Acceptable',
-			407 => 'Proxy Authentication Required',
-			408 => 'Request Timeout',
-			409 => 'Conflict',
-			410 => 'Gone',
-			411 => 'Length Required',
-			412 => 'Precondition Failed',
-			413 => 'Request Entity Too Large',
-			414 => 'Request-URI Too Long',
-			415 => 'Unsupported Media Type',
-			416 => 'Requested Range Not Satisfiable',
-			417 => 'Expectation Failed',
-			500 => 'Internal Server Error',
-			501 => 'Not Implemented',
-			502 => 'Bad Gateway',
-			503 => 'Service Unavailable',
-			504 => 'Gateway Timeout',
-			505 => 'HTTP Version Not Supported' );
+				100 => 'Continue',
+				101 => 'Switching Protocols',
+				200 => 'OK',
+				201 => 'Created',
+				202 => 'Accepted',
+				203 => 'Non-Authoritative Information',
+				204 => 'No Content',
+				205 => 'Reset Content',
+				206 => 'Partial Content',
+				300 => 'Multiple Choices',
+				301 => 'Moved Permanently',
+				302 => 'Found',
+				303 => 'See Other',
+				304 => 'Not Modified',
+				305 => 'Use Proxy',
+				306 => '(Unused)',
+				307 => 'Temporary Redirect',
+				400 => 'Bad Request',
+				401 => 'Unauthorized',
+				402 => 'Payment Required',
+				403 => 'Forbidden',
+				404 => 'Not Found',
+				405 => 'Method Not Allowed',
+				406 => 'Not Acceptable',
+				407 => 'Proxy Authentication Required',
+				408 => 'Request Timeout',
+				409 => 'Conflict',
+				410 => 'Gone',
+				411 => 'Length Required',
+				412 => 'Precondition Failed',
+				413 => 'Request Entity Too Large',
+				414 => 'Request-URI Too Long',
+				415 => 'Unsupported Media Type',
+				416 => 'Requested Range Not Satisfiable',
+				417 => 'Expectation Failed',
+				500 => 'Internal Server Error',
+				501 => 'Not Implemented',
+				502 => 'Bad Gateway',
+				503 => 'Service Unavailable',
+				504 => 'Gateway Timeout',
+				505 => 'HTTP Version Not Supported' );
 		return ( $status[$this->_code] ) ? $status[$this->_code] : $status[500];
 	}
 	public function get_request_method() {
@@ -78,20 +78,20 @@ class Rest {
 	}
 	private function inputs() {
 		switch ( $this->get_request_method() ) {
-		case "POST":
-			$this->_request = $this->cleanInputs( $_POST );
-			break;
-		case "GET":
-		case "DELETE":
-			$this->_request = $this->cleanInputs( $_GET );
-			break;
-		case "PUT":
-			parse_str( file_get_contents( "php://input" ), $this->_request );
-			$this->_request = $this->cleanInputs( $this->_request );
-			break;
-		default:
-			$this->response( '', 406 );
-			break;
+			case "POST":
+				$this->_request = $this->cleanInputs( $_POST );
+				break;
+			case "GET":
+			case "DELETE":
+				$this->_request = $this->cleanInputs( $_GET );
+				break;
+			case "PUT":
+				parse_str( file_get_contents( "php://input" ), $this->_request );
+				$this->_request = $this->cleanInputs( $this->_request );
+				break;
+			default:
+				$this->response( '', 406 );
+				break;
 		}
 	}
 	private function cleanInputs( $data ) {
