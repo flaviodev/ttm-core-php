3fd2f8154cc1579bd3f2a15afd3c9dc61cbad222
flavio <flavio@flavio>
2016-11-03 00:25:37 -0200

    comments

--- src/exception/TTMException.php
+++ src/exception/TTMException.php
@@ -2,6 +2,18 @@
 
 namespace ttm\exception;
 
+/**
+ * @author flaviodev - Flávio de Souza TTM/ITS - fdsdev@gmail.com
+ *
+ * TMMException - Exception class to centralize the treatement of exceptions
+ * on the system
+ *
+ * @see \Exception
+ *
+ * @package ttm-core-php
+ * @namespace ttm\exception
+ * @version 1.0
+ */
 class TTMException extends \Exception {
 	
 }
