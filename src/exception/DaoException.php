766205e172fb6dabd2c7569119cfa72c5aa61ecf
flavio <flavio@flavio>
2016-11-03 00:25:21 -0200

    comments

--- src/exception/DaoException.php
+++ src/exception/DaoException.php
@@ -2,5 +2,16 @@
 
 namespace ttm\exception;
 
+/**
+ * @author flaviodev - Flávio de Souza TTM/ITS - fdsdev@gmail.com
+ *
+ * DaoException - Exception class to the treatement of data base operations exceptions
+ *
+ * @see TTMExceptions
+ *
+ * @package ttm-core-php
+ * @namespace ttm\exception
+ * @version 1.0
+ */
 class DaoException extends TTMException {
 }
