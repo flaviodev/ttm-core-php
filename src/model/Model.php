bcf30c3769079fa5c6491321d18f6c79902540de
flavio <flavio@flavio>
2016-11-03 00:23:45 -0200

    remove type argument and type return on id

--- src/model/Model.php
+++ src/model/Model.php
@@ -1,9 +1,39 @@
 <?php
 namespace ttm\model;
 
+/**
+ * @author flaviodev - Flávio de Souza TTM/ITS - fdsdev@gmail.com
+ *
+ * Model - Determines the entity type to the model classes of the system, encapsulating
+ * the commons attributes and treatments
+ * 
+ * @package ttm-core-php
+ * @namespace ttm\model
+ * @abstract
+ * @version 1.0
+ */
 abstract class Model {
-	public abstract function getId();
 	
+	/**
+	 * @method getId - return the entity id (of mapped attribute on concrete class)
+	 *
+	 * @return entity id (primary key)
+	 *
+	 * @abstract
+	 * @access public
+	 * @since 1.0
+	 */
+	public abstract function getId();
+
+	/**
+	 * @method setId - set the entity id (of mapped attribute on concrete class)
+	 *
+	 * @param $id - entity id (primary key)
+	 *
+	 * @abstract
+	 * @access public
+	 * @since 1.0
+	 */
 	public abstract function setId($id);
 }
 
