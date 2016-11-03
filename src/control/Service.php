7c9bb1e3556676749bd4a5c9df8db532c122a5a2
flavio <flavio@flavio>
2016-11-03 17:11:30 -0200

    creation of interface service

---
+++ src/control/Service.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ttm\control;
+
+/**
+ * @author flaviodev - Flávio de Souza TTM/ITS - fdsdev@gmail.com
+ *
+ * Interface Service - defines a stereotype for offering a type of service to the 
+ * view layer. This stereotype was designed for implementing a concept of the 
+ * service that performs alterations data business of the system. 
+ * 
+ *  All offerd service should be created base on a service interface implementing an uncoupled 
+ *  resource, that can be changed with low inpact. This service interface should be extends 
+ *  this interface (ttm\control\Service) 
+ *
+ * @package ttm-core-php
+ * @namespace ttm\control
+ * @abstract
+ * @version 1.0
+ */
+interface Service {
+
+}
